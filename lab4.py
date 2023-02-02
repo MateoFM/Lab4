6a8c9285b059e0c6af3e9e4de682c21b78e27d02
MateoFM <122948171+MateoFM@users.noreply.github.com>
2023-02-01 17:26:42 -0800

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -3,7 +3,13 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def first_element(list_in:  list[list[int]]):
+    answer_list = []
+    for x in list_in:
+        if len(x) > 0:
+            first_item = x[0]
+            answer_list.append(first_item)
+    return(answer_list)
 
 # Part 2
 
